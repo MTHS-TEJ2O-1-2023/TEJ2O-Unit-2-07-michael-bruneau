42a862b473b8ff1b788764800d513b94103addd2
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-03 21:33:57 +0000

    Python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael B
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+number_of_cookies_clicked = 0
+
+while True:
+    if button_a.is_pressed():
+        number_of_cookies_clicked = number_of_cookies_clicked + 1
+        display.show(number_of_cookies_clicked)
+
+    if button_b.is_pressed():
+        number_of_cookies_clicked = 0
+        display.show(number_of_cookies_clicked)
